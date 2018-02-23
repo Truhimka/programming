cdb9fd81224b4e4576800853236fce6763233099
mytriv <34720398+mytriv@users.noreply.github.com>
2018-02-23 17:09:00 +0200

    Add files via upload

---
+++ python/homework2.py
@@ -0,0 +1,10 @@
+print("Task 1")
+text = "i wish i could eat your cancer when you turn black"
+while "." in text:
+    print(text.capitalize())
+    break
+else:
+    print(text.capitalize()+".")
+
+
+
